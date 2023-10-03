9b868069e0ed44b2e9bdaaf4eaee287ba5e786cc
cris-andrade-97 <cristian201097@gmail.com>
2023-10-03 13:54:31 -0300

    Headers de Linux

--- mineria.py
+++ mineria.py
@@ -7,7 +7,7 @@ import warnings
 
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
-HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"}
+HEADER = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
 
 ultimaPagina = False
 primerRegistro = True
